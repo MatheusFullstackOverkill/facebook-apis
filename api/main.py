9f0063417e4919df7e4d2f8f298a20d182d961dd
Matheus <matheudbaf0123@gmail.com>
2022-09-15 09:43:06 -0300

    WIP

--- api/main.py
+++ api/main.py
@@ -4,11 +4,11 @@ from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['GET', 'POST'])
+@app.route('/api/hook', methods=['GET', 'POST'])
 def webhook():
     print(request)
     try:
         print(request.get_json())
     except Exception as e:
         print(e)
-    return Response(response={'message': 'success'}, status=200)
+    return Response(response={'message': '1836548521'}, status=200)
